1e6e9ddbeacd32d12a725a08e1c3e760543adfb5
Nicholas Moore <nicholasvmoore@gmail.com>
2015-07-19 19:47:31 -0700

    updated the start.sh, debugging an issue

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ export PLEX_MEDIA_SERVER_HOME=/usr/lib/plexmediaserver
 export PLEX_MEDIA_SERVER_MAX_PLUGIN_PROCS=6
 export PLEX_MEDIA_SERVER_TMPDIR=/tmp
 export LD_LIBRARY_PATH=/usr/lib/plexmediaserver
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+#export LC_ALL=en_US.UTF-8
+#export LANG=en_US.UTF-8
 /bin/sh -c '/usr/bin/test -d "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}" || /bin/mkdir -p "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}"'
 /bin/sh -c '/usr/lib/plexmediaserver/Plex\ Media\ Server'
